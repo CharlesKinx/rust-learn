8ddfe960438fc650ed8404bea2a6be7a3b90f533
WuHD <CharlesKinx@outlook.com>
2024-04-28 18:46:26 +0800

    更新rust

---
+++ src/gener/trait_object.rs
@@ -0,0 +1,51 @@
+pub trait Draw {
+    pub fn draw(&self);
+}
+
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // 绘制按钮的代码
+        println!("绘制Button...");
+    }
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("绘制SelectBox...");
+    }
+}
+pub struct Screen {
+    pub components: Vec<Draw>,
+}
+
+
+fn draw2(x: &dyn Draw) {
+    x.draw();
+}
+
+fn main() {
+    let x = 1.1f64;
+    // do_something(&x);
+    let y = 8u8;
+
+    // x 和 y 的类型 T 都实现了 `Draw` 特征，因为 Box<T> 可以在函数调用时隐式地被转换为特征对象 Box<dyn Draw> 
+    // 基于 x 的值创建一个 Box<f64> 类型的智能指针，指针指向的数据被放置在了堆上
+    draw1(Box::new(x));
+    // 基于 y 的值创建一个 Box<u8> 类型的智能指针
+    draw1(Box::new(y));
+    draw2(&x);
+    draw2(&y);
+}
