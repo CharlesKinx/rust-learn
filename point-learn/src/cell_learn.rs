dee9b8cbe46b0512c99a5a03b2105587f44ec041
WuHD <CharlesKinx@outlook.com>
2024-05-02 18:27:08 +0800

    recell与cell学习

--- point-learn/src/cell_learn.rs
+++ point-learn/src/cell_learn.rs
@@ -1,10 +1,34 @@
 
 use std::cell::Cell;
-
+use std::cell::RefCell;
 pub fn print_cell(){
     let c = Cell::new("asdf");
     let one = c.get();
     c.set("qaz");
     let two = c.get();
-    println!("{},{}",one,two);
+    let three = one;
+    println!("{},{},{}",one,two,three);
+
+}
+
+pub fn print_recell() {
+    let s = RefCell::new(String::from("hello, world"));
+    let s1 = s.borrow();
+    let s2 = s.borrow_mut();
+    println!("{},{}", s1, s2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_test(){
+        print_cell();
+    }
+
+    #[test]
+    fn two_test(){
+        print_recell();
+    }
 }
---
+++ point-learn/target/debug/deps/point_learn-c22643b34353d929.d
@@ -0,0 +1,7 @@
+C:\Users\WuHD\Desktop\rust\rust_learn\point-learn\target\debug\deps\point_learn-c22643b34353d929.exe: src\main.rs src\dr_learn.rs src\cell_learn.rs
+
+C:\Users\WuHD\Desktop\rust\rust_learn\point-learn\target\debug\deps\point_learn-c22643b34353d929.d: src\main.rs src\dr_learn.rs src\cell_learn.rs
+
+src\main.rs:
+src\dr_learn.rs:
+src\cell_learn.rs:
